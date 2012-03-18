a0b960c102e6cb57bd045d91b3411f93b6bd3b5c
Chih-Wei Lee <dlackty@gmail.com>
2012-03-18 20:07:17 +0800

    Scale image of photo and add thumb version.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,25 +16,15 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  # Resize as they are uploaded
+  process :resize_to_fit => [640, 640]
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  # Create thumb version of uploade file
+  version :thumb do
+    process :resize_to_fit => [320, 320]
+  end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+  # Whitelist
   def extension_white_list
     %w(jpg jpeg)
   end
